dd5d0c81052bd41f29e24d58375c8561ce0e2676
subaek0704 <subaek0704@dongguk.edu>
2022-05-21 13:02:59 +0900

    이름학번

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("이름을 입력하세요: ");
+        String name = sc.next();
+        System.out.print("학번을 입력하세요: ");
+        int num = sc.nextInt();
+        sc.close();
+
+        System.out.println();
+        System.out.println("<출력>");
+        System.out.println("이름: "+name);
+        System.out.println("학번: "+num);
+    }
+}
